9de19928349a15f39a437fc783d4350e29d4bc01
Aleksandr Ryabikov <rsashka@mail.ru>
2026-08-20 20:04:25 +0300

    New site in MAINTENANCE MODE + playground 0.6.0 + playground fix + docs

--- include/precompiled/torch_pch.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef TORCH_PCH_HPP
-#define TORCH_PCH_HPP
-
-#include <torch/torch.h>
-
-#endif // TORCH_PCH_HPP
--- test/unit/types/numbers_test.cpp
+++
@@ -1,6 +0,0 @@
-// Test file disabled: Numbers/Float runtime type was removed.
-// Restore when the new type system is completed.
-//
-// #include <gtest/gtest.h>
-// #include "types/forward.hpp"
-// #include "types/numbers.hpp"
